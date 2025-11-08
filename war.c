00c95158752ab27635a2ec2d8f4b2da1bcd61d05
Vitor Lima <vitorldo1@gmail.com>
2025-11-07 22:54:57 -0300

    criacao do menu dos territorios

--- war.c
+++ war.c
@@ -11,13 +11,17 @@ struct Territorio territorios[5];
 
 int main()
 {
-    strcpy(territorios[0].nome, "America");
-    strcpy(territorios[0].cor, "Azul");
-    territorios[0].tropas = 3;
-    printf("%s\n", territorios[0].nome);
-
-    territorios[1] = (struct Territorio){"Oceania", "Amarelo", 5};
-    printf("%s\n", territorios[1].nome);
-    
+    // Criacao do Menu
+    printf("=============================\n\n");
+    printf("Vamos cadastrar os 5 territorios iniciais do nosso mundo.\n");
+    for (int i = 1; i<=5; i++) {
+        printf("\n--- Cadastrando Territorio %d ---\n", i);
+        printf("Nome do Territorio: ");
+        scanf("%s", territorios[i-1].nome);
+        printf("Cor do Exercito: ");
+        scanf("%s", territorios[i-1].cor);
+        printf("Numero de Tropas: ");
+        scanf("%d", &territorios[i-1].tropas);
+    }
     return 0;
 }
