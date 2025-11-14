ed20e81eb56ee365196e10cf577687e6d4249dcb
Vitor Lima <vitorldo1@gmail.com>
2025-11-14 15:15:33 -0300

    criado a estrutura para Escolha de ataque

--- war.c
+++ war.c
@@ -28,28 +28,45 @@ int main()
         scanf("%s", t[i-1].cor);
         printf("Numero de Tropas: ");
         scanf("%d", &t[i-1].tropas);
-        
-    }
+    }   
     //Exibe os 5 territórios cadastrados 
     printf("=========================================\n");
     printf("      MAPA DO MUNDO - ESTADO ATUAL       \n");
     printf("=========================================\n");
-    //Loop de apresentacao de todos os territorios
-        for (int i = 1; i< numTerritorio; i++) {
-            printf("%d. ", i );
-            printf("%s", t[i-1].nome);
-            printf(" (Exército %s,", t[i-1].cor);
-            printf("Tropas: %d)\n", t[i-1].tropas);
-            printf("\n"); 
-        }
-        //fase de ataque 
-       // printf("--- FASE DE ATAQUE---"); 
-        // Função para jogar dados 
-        //int lancadados() {
-            //return num.aleatório(1,6);
-        //int dadoAtacante = lancadados();
-        //int dadoDefensor = lancadados();
-        free(t);
-        return 0;
+    
+    for (int i = 1; i <= numTerritorio; i++) {
+    printf("%d. %s (Exército %s, Tropas: %d)\n", i, t[i-1].nome, t[i-1].cor, t[i-1].tropas);
     }
+    //Loop de apresentacao de todos os territorios
+    // Fase de ataque
+    
+    printf("\n=========================================\n");
+    printf("         FASE DE ATAQUE                  \n");
+    printf("=========================================\n");
+    
+    int atacante, defensor;
 
+    printf("\n--- ESCOLHA NUMERO DO TERRITORIO ATACANTE ---\n");
+        
+        while (1) {  // 1 = sempre verdadeiro, loop continua
+            printf("Digite um numero (1 a 5): ");
+            scanf("%d", &atacante);
+            
+            if (atacante >= 1 && atacante <= 5) {
+                printf("Valido!\n");
+                break;  // Para o loop (sai do while)
+            } else {
+                printf("Invalido! Tente novamente.\n");
+                while (getchar() != '\n'); // Limpa até encontrar quebra de linha
+                // Volta para o início do while(1)
+            
+        }
+    }
+         
+
+
+
+        free(t);
+         return 0;
+}  
+    
